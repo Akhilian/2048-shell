27c0f29af04a37342ada6f738c832934a4b50db9
SAUNIER Adrien <sauniera@mac12-m102.utt.fr>
2014-03-25 18:04:47 +0100

    Display the grid from a string

--- Display.sh
+++ Display.sh
@@ -20,3 +20,26 @@ display_help() {
     echo "HOW TO PLAY: Use your arrow keys to move the tiles. When two tiles with the same number touch, they merge into one!"
 
 }
+
+display_grid() {
+
+    if [ -z $1 ]; then
+        echo "No string specified.";
+        exit
+    else
+        string=$1
+        while test -n "$string"; do
+            c=${string:0:1}
+
+            
+
+            # Removing the first character of the string
+            string=${string:1}
+        done
+    fi
+
+}
+
+chaine="0-0-0-0|0-0-0-0|0-0-0-0|0-0-0-0"
+
+display_grid $chaine
